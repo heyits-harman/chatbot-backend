4b7b78e1c665ff462e975375b6c8ca77582093c2
heyits-harman <harrygill7664@gmail.com>
2025-11-15 12:31:23 +0530

    Update CORS for Vercel

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
  
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'], 
+  origin: ['http://localhost:3000', 'https://chatbot-frontend-beryl-delta.vercel.app/'], 
   credentials: true
 }));
 
